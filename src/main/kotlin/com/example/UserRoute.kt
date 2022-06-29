9c287d5bdfe2ef8e34119eb060257af6d7d74562
felixmaina <karani.maina2010@gmail.com>
2022-06-29 16:21:49 +0300

    get all the users in the mutable list

--- src/main/kotlin/com/example/UserRoute.kt
+++ src/main/kotlin/com/example/UserRoute.kt
@@ -1,9 +1,19 @@
 package com.example
 
+import com.example.model.userStorage
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.userRoute() {
-    get{}
+    get{
+        if (userStorage.isNotEmpty()) {
+            call.respond(userStorage)
+        } else {
+            call.respondText("No users found", status = HttpStatusCode.OK)
+        }
+    }
     get("{id}"){
 
     }
