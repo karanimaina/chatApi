8917f2c3e5ac4dc68e0bde3663ef59a5cfd7b6bf
felixmaina <karani.maina2010@gmail.com>
2022-06-29 16:45:08 +0300

    add an a delete and post api request

--- src/main/kotlin/com/example/UserRoute.kt
+++ src/main/kotlin/com/example/UserRoute.kt
@@ -1,8 +1,10 @@
 package com.example
 
+import com.example.model.User
 import com.example.model.userStorage
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -15,12 +17,23 @@ fun Route.userRoute() {
         }
     }
     get("{id}"){
+  val id  = call.parameters["id"]?:return@get call.respondText ("missing id ", status = HttpStatusCode.OK )
+        val user = userStorage.find { it.id== id }?: return@get call.respondText("No users with the given Id", status = HttpStatusCode.NotFound)
+        call.respond(user)
 
     }
-    post {
 
+    post {
+    val user  = call.receive<User>()
+        userStorage.add(user)
+        call.respondText("User stored success fully", status = HttpStatusCode.Created)
     }
     delete("{id?}") {
-
-    }
+     val id = call.parameters["id"]?:call.respond(HttpStatusCode.BadRequest)
+        if (userStorage.removeIf{it.id==id }){
+            call.respondText  ("user removed successfully")
+        }else {
+            call.respondText  ("user Not found" , status = HttpStatusCode.NotFound)
+        }
+}
 }
