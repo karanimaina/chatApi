606f4030f1925ef608f2b0d0cd4f5b3fa97ca713
felixmaina <karani.maina2010@gmail.com>
2022-06-29 16:12:30 +0300

    add  user dataclass

---
+++ src/main/kotlin/com/example/model/User.kt
@@ -0,0 +1,5 @@
+package com.example.model
+
+data class User(val id:String,val firstName:String, val lastName:String, val profession:String,val age:String,val maritalStatus:String,val hobby:String, val email:String)
+
+val userStorage = mutableListOf<User>()
