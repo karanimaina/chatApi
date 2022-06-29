23a56479908c381f90d05394618e314215005605
felixmaina <karani.maina2010@gmail.com>
2022-06-29 16:47:31 +0300

    set the user routein the configure routing of the application module

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import com.example.userRoute
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -9,6 +10,7 @@ import io.ktor.server.request.*
 fun Application.configureRouting() {
 
     routing {
+        userRoute()
 
     }
 }
